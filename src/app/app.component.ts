efffdb6119ee3b8a2fda1984ff216502301807a4
Quentin Petton <quentin.petton@hotmail.fr>
2025-03-03 16:44:42 +0100

    fix css for submit challenge solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from "./components/product-list/product-list.component";
-import { CartComponent } from "./components/cart/cart.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductListComponent, CartComponent],
+  imports: [ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
